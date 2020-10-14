829d01d2c0857ae2b2f242f515779e3f844c97f7
Melissa <thecodeplanner@gmail.com>
2020-10-14 02:43:16 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,11 +8,10 @@
 
 def select_winner(passengers)
   winner = ""
-passengers.each do |suite, name|
-  if suite == :suite_a && name.start_with?("A")
-    winner = name
+  passengers.each do |suite, name|
+    if suite == :suite_a && name.start_with?("A")
+      winner = name
+    end
   end
-end
- 
-winner
+  winner
 end
